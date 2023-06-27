27f2eb34ae7e1aa6bebbffc301827230c35f2151
Gregory Grubbs <gregory@dynapse.com>
2023-06-27 17:04:31 -0600

    Print title of newly created KB article

--- kb_microservice.py
+++ kb_microservice.py
@@ -37,21 +37,15 @@ def get_text_from_url(url):
     # Kill all script and style elements
     for script in soup(["script", "style"]):
         script.decompose()
-
     # Get text
     text = soup.get_text()
-
     # Break into lines and remove leading and trailing spaces on each line
     lines = (line.strip() for line in text.splitlines())
-
     # Break multi-headlines into a line each
     chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-
     # Drop blank lines
     text = '\n'.join(chunk for chunk in chunks if chunk)
-
     return text
-
 
 ###     chatbot functions
 
@@ -82,9 +76,6 @@ def chatbot(messages, model="gpt-4", temperature=0):
 
 ###     KB functions
 
-
-
-
 def update_directory():
     kb_dir = 'kb/'
     directory = ''
@@ -95,17 +86,12 @@ def update_directory():
             directory += '\n%s - %s - %s - %s\n' % (filename, kb.get_file_property('title'), kb[1].body.lstrip('\n').split('\n')[0], ', '.join(kb.get_file_property_list('filetags')))
     save_file('directory.txt', directory.strip())
 
-
-
 def search_kb(query):
     directory = open_file('directory.txt')
     system = open_file('system_search.txt').replace('<<DIRECTORY>>', directory)
     messages = [{'role': 'system', 'content': system}, {'role': 'user', 'content': query}]
     response, tokens, model = chatbot(messages)
     return json.loads(response)
-
-# def create_article_url(url):
-#     text = get_text_from_url(url)
 
 def create_article(text='', url='', model='gpt-4', temperature=0):
     # If url is specified, text value is ignored
@@ -118,7 +104,9 @@ def create_article(text='', url='', model='gpt-4', temperature=0):
     response, tokens, model = chatbot(messages, model="gpt-4", temperature=0.5)  # response will be Org Mode document string
     extra_meta = '#+source: %s\n#+identifier: abcde\n#+created: [%s]\n#+model: %s\n#+setupfile: ~/projects/emacs/org-html-themes/org/theme-readtheorg-local.setup\n' % (source, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), model)
     save_file('kb/%s.org' % str(uuid.uuid4()), extra_meta + response)
-    print('CREATE KB')
+    orgparse.loads(response)[1].heading
+
+    print('Created KB', orgparse.loads(response)[1].heading)
     # print('CREATE', kb['title'])
 
 
